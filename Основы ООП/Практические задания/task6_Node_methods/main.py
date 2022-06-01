9eb7d5625cfcc64e76a5fc6e6a3010f1c5fd814b
russos <ro0ne@mail.ru>
2022-06-01 19:46:20 +0300

    ghost

--- Основы ООП/Практические задания/task6_Node_methods/main.py
+++ Основы ООП/Практические задания/task6_Node_methods/main.py
@@ -4,7 +4,7 @@ from typing import Any, Optional
 class Node:
     """ Класс, который описывает узел связного списка. """
 
-    def __init__(self, value: Any, next_: Optional["Node"] = None):
+    def __init__(self, value: Any, next_: Optional["Node"] = None): # если убрать ковычки то выдаст ошибку
         """
         Создаем новый узел для односвязного списка
         :param value: Любое значение, которое помещено в узел
