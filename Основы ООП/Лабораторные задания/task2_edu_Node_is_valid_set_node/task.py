c682839df27f3a2ed032a22f81976133b7be5b3b
russos <ro0ne@mail.ru>
2022-06-01 21:06:01 +0300

    не работает(

--- Основы ООП/Лабораторные задания/task2_edu_Node_is_valid_set_node/task.py
+++ Основы ООП/Лабораторные задания/task2_edu_Node_is_valid_set_node/task.py
@@ -11,24 +11,28 @@ class Node:
         :param next_: следующий узел, если он есть
         """
         self.value = value
-
-        # TODO установить значение следующего узла с помощью метода set_next
+        self.next = None
+        self.set_next = next_
 
     def __repr__(self) -> str:
         return f"Node({self.value}, {self.next})"
 
     def is_valid(self, node: Any) -> None:
        # TODO метод проверки корректности связываемого узла
-        ...
+       if not isinstance(self.next, node):
+           raise TypeError
 
     def set_next(self, next_: Optional["Node"] = None) -> None:
         # TODO метод должен проверять корректность узла и устанавливать значение атрибуту next
-        ...
+        self.is_valid(next_)
+        self.next = next_
 
 
 if __name__ == '__main__':
     # TODO инициализируйте два узла с любыми значеними
-
+    first_node = Node(1)
+    second_node = Node(2)
+    first_node.next = second_node
     # TODO свяжите первый узел со вторым
 
     print(first_node)
