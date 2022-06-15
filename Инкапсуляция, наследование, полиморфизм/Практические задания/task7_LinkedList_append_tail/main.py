5cd649968dd341c818db5d1801c882f32ba58193
russos <ro0ne@mail.ru>
2022-06-15 18:57:38 +0300

    no done

--- Инкапсуляция, наследование, полиморфизм/Практические задания/task7_LinkedList_append_tail/main.py
+++ Инкапсуляция, наследование, полиморфизм/Практические задания/task7_LinkedList_append_tail/main.py
@@ -8,7 +8,7 @@ class LinkedList:
         """Конструктор связного списка"""
         self.len = 0
         self.head: Optional[Node] = None
-        ...  # TODO добавить атрибут tail
+        self.tail: Optional[Node] = None # TODO добавить атрибут tail
 
         if data is not None:
             for value in data:
@@ -19,12 +19,12 @@ class LinkedList:
         append_node = Node(value)
 
         if self.head is None:
-            self.head = append_node  # TODO добавить использование self.tail
+            self.head = self.tail = append_node  # TODO добавить использование self.tail
         else:
-            last_index = self.len - 1  # TODO Переделать через атрибут tail
-            last_node = self.step_by_step_on_nodes(last_index)
-
+            #last_index = self.len - 1  # TODO Переделать через атрибут tail
+            last_node = self.tail# добавить  tail после self step_by_step_on_nodes(last_index)
             self.linked_nodes(last_node, append_node)
+            self.tail = append_node
 
         self.len += 1
 
