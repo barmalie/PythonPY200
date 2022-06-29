dc6b58713aac65ac2bc725b53e7b11c46199091a
russos <ro0ne@mail.ru>
2022-06-29 19:15:57 +0300

    работает

--- Управление памятью, множественное наследование/Практические задания/task4_Node_test_case/test_task.py
+++ Управление памятью, множественное наследование/Практические задания/task4_Node_test_case/test_task.py
@@ -3,20 +3,30 @@ import unittest
 from task import Node
 
 
-class TestCase(unittest):  # TODO наследоваться от unittest.TestCase
+class TestCase(unittest.TestCase):
     def test_init_node_without_next(self):
         """Проверить следующий узел после инициализации с аргументом next_ по умолчанию"""
-        ...  # TODO с помощью метода assertIsNone проверить следующий узел
-
+        node = Node(5)# TODO с помощью метода assertIsNone проверить следующий узел
+        self.assertIsNone(node.next,
+                          msg="При инициализации значение по умолчанию следующего узла не None")
     def test_init_node_with_next(self):
         """Проверить следующий узел после инициализации с переданным аргументом next_"""
-        ...  # TODO проверить что узлы связались
+        second_node = Node("second_node")
+        first_node = Node("first_node", next_=second_node)  # TODO проверить что узлы связались
+        expected_value = second_node
+        actual_value = first_node.next
+        self.assertIs(actual_value, expected_value, msg="узлы не эквиваленты")
 
     def test_repr_node_without_next(self):
         """Проверить метод __repr__, для случая когда нет следующего узла."""
-        ...  # TODO проверить метод __repr__ без следующего узла
+        node_value = 5
+        node = Node(node_value)# TODO проверить метод __repr__ без следующего узла
+        expected_value = f"Node({node_value}, None)"
+        actual_value = repr(node)
+        self.assertEqual(expected_value, actual_value,
+                         msg="неверный repr для node следующего узла")
 
-    ...  # TODO пропустить тест с помощью декоратора unittest.skip
+    @unittest.skip(reason="Еще не реализованная функциональность") # TODO пропустить тест с помощью декоратора unittest.skip
     def test_repr_node_with_next(self):
         """Проверить метод __repr__, для случая когда установлен следующий узел."""
         ...
