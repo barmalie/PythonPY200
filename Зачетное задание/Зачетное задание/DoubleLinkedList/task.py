de25576ffc66b3c1866d300e3c750041ce249045
russos <ro0ne@mail.ru>
2022-07-06 18:32:09 +0300

    работает

--- Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
@@ -1,10 +1,98 @@
+from typing import Any, Iterable, Optional
+
+from node import Node
+
 class LinkedList:
-    ...
+    def __init__(self, data: Iterable = None):
+        """Конструктор связного списка"""
+        self.len = 0
+        self.head: Optional[Node] = None
+        self.tail: Optional[Node] = None
+
+        if data is not None:
+            for value in data:
+                self.append(value)
+
+    def append(self, value: Any):
+        """ Добавление элемента в конец связного списка. """
+        append_node = Node(value)
+
+        if self.head is None:
+            self.head = self.tail = append_node
+        else:
+            # last_index = self.len - 1
+            last_node = self.tail  # добавить  tail после self step_by_step_on_nodes(last_index)
+            self.linked_nodes(last_node, append_node)
+            self.tail = append_node
+
+        self.len += 1
+
+    def step_by_step_on_nodes(self, index: int) -> Node:
+        """ Функция выполняет перемещение по узлам до указанного индекса. И возвращает узел. """
+        if not isinstance(index, int):
+            raise TypeError()
+
+        if not 0 <= index < self.len:  # для for
+            raise IndexError()
+
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+
+        return current_node
+
+    @staticmethod
+    def linked_nodes(left_node: Node, right_node: Optional[Node] = None) -> None:
+        """
+        Функция, которая связывает между собой два узла.
+
+        :param left_node: Левый или предыдущий узел
+        :param right_node: Правый или следующий узел
+        """
+        left_node.set_next(right_node)
+
+    def __getitem__(self, index: int) -> Any:
+        """ Метод возвращает значение узла по указанному индексу. """
+        node = self.step_by_step_on_nodes(index)
+        return node.value
+
+    def __setitem__(self, index: int, value: Any) -> None:
+        """ Метод устанавливает значение узла по указанному индексу. """
+        node = self.step_by_step_on_nodes(index)
+        node.value = value
+
+    def to_list(self) -> list:
+        return [linked_list_value for linked_list_value in self]
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.to_list()})"
+
+    def __str__(self) -> str:
+        return f"{self.to_list()}"
 
 
 class DoubleLinkedList(LinkedList):
-    ...
+    @staticmethod
+    def linked_nodes(left_node: DoubleLinkedNode, right_node: Optional[DoubleLinkedNode] = None) -> None:
+        left_node.next = right_node
+        right_node.prev = left_node
+
+    def append(self, value: Any):
+        """ Добавление элемента в конец связного списка. """
+        append_node = DoubleLinkedNode(value)
+
+        if self.head is None:
+            self.head = self.tail = append_node
+        else:
+            self.linked_nodes(self.tail, append_node)
+            self.tail = append_node
+
+        self.len += 1
 
 
 if __name__ == "__main__":
-    ...
+    ll = DoubleLinkedList([1, 2, 3, 4, 5])
+
+    ll.clear()
+
+    print(ll)
